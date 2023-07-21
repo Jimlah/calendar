998e36e80477925f1d1b194c39c2e9c9c73835fe
Abdullahi Jimoh <abdullahij951@gmail.com>
2023-07-21 17:00:52 +0100

    added

--- resources/views/components/period/day.blade.php
+++ resources/views/components/period/day.blade.php
@@ -5,9 +5,7 @@
     </div>
     <div class="py-2 w-full h-full">
         <div class="w-full relative px-4 flex-grow overflow-y-auto h-full">
-            <span class="absolute w-full inline-block left-0 z-30 border-t border-t-red-600" x-time>
-                <span x-text='$data.time?.toLocaleTimeString("en-us", { hour: "2-digit", minute: "2-digit"}) ?? null' class="absolute text-red-600 top-0 left-0 text-xs -mt-2 bg-white inline-block whitespace-nowrap"></span>
-            </span>
+            <x-timeline />
             @foreach($periods as $period)
                 @foreach($period as $hour)
                     <div class="h-[8.3333%] w-full relative border-t last:border-b">
---
+++ resources/views/components/timeline.blade.php
@@ -0,0 +1,4 @@
+ <span class="absolute w-full inline-block left-0 z-30 border-t border-t-red-600" x-time>
+      <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+     <span x-text='$data.time?.toLocaleTimeString("en-us", { hour: "2-digit", minute: "2-digit"}) ?? null' class="absolute text-red-600 top-0 left-0 text-xs -mt-2 bg-white inline-block whitespace-nowrap"></span>
+ </span>
